17ffa10ac8c391a80621a7411681ef0ac0961638
User <User@DESKTOP-M84BVS0>
2022-03-28 21:06:31 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Elevador.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    public class Elevador
+    {
+        public int Inicializar(int capacidade, int totalAndares)
+        {
+            int codmsg = 0;
+            return codmsg;
+        }
+
+        public int Entrar(int capacidade, int nrPesAtual)
+        {
+            if (nrPesAtual < capacidade) nrPesAtual = nrPesAtual + 1;
+            else Console.WriteLine("Elevador lotou ");
+            return nrPesAtual;
+        }
+
+        public int Sair(int nrPesAtual)
+        {
+            if (nrPesAtual != 0) nrPesAtual = (nrPesAtual - 1);
+            else
+            {
+                Console.WriteLine("Elevador vazio " + nrPesAtual.ToString());
+            }
+            return nrPesAtual;
+        }
+
+        public int Subir(int andarAlvo, int totalAndares, int andarAtual)
+        {
+            if (andarAlvo > totalAndares) Console.WriteLine("Você atingiu o topo " + andarAlvo.ToString());
+            else
+            {
+                while (andarAtual != andarAlvo)
+                {
+                    andarAtual = andarAtual + 1;
+                    Console.WriteLine("Subindo " + andarAtual.ToString());
+                }
+                Console.WriteLine("Chegou no andar " + andarAtual.ToString());
+            }
+            return andarAtual;
+        }
+        public int Descer(int andarAtual, int andarAlvo)
+        {
+            while (andarAtual != andarAlvo)
+            {
+                andarAtual = andarAtual - 1;
+                Console.WriteLine("Descendo " + andarAtual.ToString());
+            }
+            Console.WriteLine("Chegou no andar " + andarAtual.ToString());
+            return andarAtual;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ProjetoElevador.Model;
+
+
+namespace ProjetoElevador
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int andarAtual = 0;
+            int nrPesAtual = 0;
+            int andarAlvo = 1;
+
+            Console.WriteLine("Informe quantos andares tem o prédio, excluindo o térreo: ");
+            int totalAndares = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Capacidade de pessoas no elevador: ");
+            int nrPessoas = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(totalAndares);
+            //Console.WriteLine(nrPessoas);
+
+            Elevador elevador = new Elevador();
+
+
+            while (andarAlvo != 0)
+            {
+                Console.WriteLine("Digite o andar que você vai: ");
+                andarAlvo = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Quantas pessoas ficaram no elevador? ");
+                //nrPesAtual = int.Parse(Console.ReadLine());
+
+                if (andarAlvo > andarAtual)
+                {
+                    nrPesAtual = elevador.Entrar(nrPessoas, nrPesAtual);
+                    andarAtual = elevador.Subir(andarAlvo, totalAndares, andarAtual);
+                }
+                if (andarAlvo < andarAtual)
+                {
+                    nrPesAtual = elevador.Entrar(nrPessoas, nrPesAtual);
+                    andarAtual = elevador.Descer(andarAtual, andarAlvo);
+                }
+                if (andarAlvo == andarAtual)
+                {
+                    nrPesAtual = elevador.Sair(nrPesAtual);
+                }
+            }
+
+        }
+    }
+}
